f6316136189b78feab789d445cfc9ede11a245a9
vagrant <vagrant@precise64.(none)>
2015-03-22 20:59:49 +0000

    initial commit

--- scores/urls.py
+++ scores/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import JsonResponse
+
+
+def process_ranking(request):
+    print request.GET.get('clase')
+    print request.GET.get('alumno')
+    print request.GET.get('ranking')
+    return JsonResponse({'message':'ok'})
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +15,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    (r'^ranking/$', process_ranking),
 )
