96c2cb2d39d4bb3ad88b548ec25aa451055fa607
vagrant <vagrant@precise64.(none)>
2015-03-22 21:11:09 +0000

    working rest example

--- scores/urls.py
+++ scores/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.http import JsonResponse
+from aula.models import Alumno
+from aula.models import Clase
+from aula.models import Ranking
 
 
 def process_ranking(request):
-    print request.GET.get('clase')
-    print request.GET.get('alumno')
-    print request.GET.get('ranking')
+    claseId = request.GET.get('clase')
+    alumnoId =  request.GET.get('alumno')
+    ranking = request.GET.get('ranking')
+    alumno = Alumno.objects.get(id=int(alumnoId))
+    clase = Clase.objects.get(id=int(claseId))
+    to_save = Ranking(valor=int(ranking), alumno = alumno, clase= clase)
+    to_save.save()
     return JsonResponse({'message':'ok'})
 
 urlpatterns = patterns('',
